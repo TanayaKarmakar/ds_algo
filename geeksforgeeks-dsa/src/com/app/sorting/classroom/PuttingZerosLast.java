b51456bc6d1118b92b86fdea018fe8284dd8d44f
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2019-11-30 22:53:30 +0530

    sorting

--- geeksforgeeks-dsa/src/com/app/sorting/classroom/PuttingZerosLast.java
+++ geeksforgeeks-dsa/src/com/app/sorting/classroom/PuttingZerosLast.java
@@ -3,6 +3,25 @@ package com.app.sorting.classroom;
 import java.util.Arrays;
 
 public class PuttingZerosLast {
+	private static void anotherApproach(int[] arr) {
+		int i = -1;
+
+		int n = arr.length;
+
+		int j = 0;
+		while (j < n) {
+			if (arr[j] > 0) {
+				i++;
+				arr[i] = arr[j];
+			}
+			j++;
+		}
+
+		for (j = i + 1; j < n; j++) {
+			arr[j] = 0;
+		}
+	}
+
 	private static void sortArray(int[] arr) {
 		int n = arr.length;
 
@@ -24,9 +43,15 @@ public class PuttingZerosLast {
 	public static void main(String[] args) {
 		int[] arr = { 0, 1, 0, 2, 0, 0, 0, 4, 6, 2, 8, 0, 10, 8, 0, 2, 6, 7 };
 		sortArray(arr);
-		
+
 		System.out.println("After sorting");
 		System.out.println(Arrays.toString(arr));
+		
+		int[] arr1 = { 0, 1, 0, 2, 0, 0, 0, 4, 6, 2, 8, 0, 10, 8, 0, 2, 6, 7 };
+		anotherApproach(arr1);
+		
+		System.out.println("After arrangement");
+		System.out.println(Arrays.toString(arr1));
 	}
 
 }
