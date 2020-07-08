3052b38d83ff3e08ff632327a1aa61d7cbf1e0d2
Tanaya Karmakar <karmakar.tanaya@gmail.com>
2020-07-08 08:02:34 +0530

    code

--- leetcode/src/com/app/questions/array/DeckOfCardsLeetcode914.java
+++ leetcode/src/com/app/questions/array/DeckOfCardsLeetcode914.java
@@ -5,29 +5,39 @@ import java.util.Map;
 
 public class DeckOfCardsLeetcode914 {
 	private static boolean hasGroupSizeX(int[] deck) {
-		int min = Integer.MAX_VALUE;
 		Map<Integer, Integer> map = new HashMap<>();
-		for(int num: deck) {
+		for (int num : deck) {
 			map.put(num, map.getOrDefault(num, 0) + 1);
 		}
-		
-		for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
-			min = Integer.min(min, entry.getValue());
+
+		int g = -1;
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			int val = entry.getValue();
+			if (g == -1) {
+				g = val;
+			} else {
+				g = gcd(g, val);
+			}
 		}
-		
-		if(min < 2)
-			return false;
-		
-		for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
-			if(entry.getValue() % min != 0)
-				return false;
-		}
-		return true;
+		return g >= 2 ? true : false;
+	}
+
+	private static int gcd(int x, int y) {
+		return (x == 0) ? y : gcd(y % x, x);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] deck = { 1, 2, 3, 4, 4, 3, 2, 1 };
 
+		boolean canArrange = hasGroupSizeX(deck);
+
+		System.out.println(canArrange);
+
+		deck = new int[] { 1, 1, 1, 2, 2, 2, 3, 3 };
+		
+		canArrange = hasGroupSizeX(deck);
+
+		System.out.println(canArrange);
 	}
 
 }
--- leetcode/src/com/app/questions/trie/SearchSuggestionSystemLeetcode1268.java
+++ leetcode/src/com/app/questions/trie/SearchSuggestionSystemLeetcode1268.java
@@ -1,25 +1,104 @@
 package com.app.questions.trie;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class TrieNode1 {
 	boolean isEnd;
 	TrieNode1[] children;
-	
+
 	public TrieNode1() {
 		children = new TrieNode1[26];
 	}
 }
 
 public class SearchSuggestionSystemLeetcode1268 {
-	
-	
-	public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-		return null;
+	private static void insert(TrieNode1 trie, String word) {
+		TrieNode1 curr = trie;
+		for (int i = 0; i < word.length(); i++) {
+			int indx = word.charAt(i) - 'a';
+			if (curr.children[indx] == null) {
+				curr.children[indx] = new TrieNode1();
+			}
+			curr = curr.children[indx];
+		}
+		curr.isEnd = true;
+	}
+
+	private static TrieNode1 buildTrie(String[] products) {
+		TrieNode1 trie = new TrieNode1();
+		for (String word : products) {
+			insert(trie, word);
+		}
+		return trie;
+	}
+
+	private static void traverseList(TrieNode1 node, String word, List<String> item) {
+		if(item.size() == 3)
+			return;
+		if (node.isEnd) {
+			item.add(word);
+		}
+		for (int i = 0; i < 26; i++) {
+			if (node.children[i] != null) {
+				String word1 = word + (char) (i + 'a');
+				traverseList(node.children[i], word1, item);
+			}
+		}	
+	}
+
+	private static List<List<String>> suggestedProducts(String[] products, String searchWord) {
+		TrieNode1 node = buildTrie(products);
+		List<List<String>> res = new ArrayList<>();
+		for (int i = 0; i < searchWord.length(); i++) {
+			String str = searchWord.substring(0, i + 1);
+			TrieNode1 curr = node;
+			boolean found = true;
+			for (int j = 0; j < str.length(); j++) {
+				int indx = str.charAt(j) - 'a';
+				if (curr.children[indx] == null) {
+					found = false;
+					break;
+				}
+				curr = curr.children[indx];
+			}
+			if (!found)
+				res.add(new ArrayList<>());
+			else {
+				List<String> item = new ArrayList<>();
+				traverseList(curr, str, item);
+				res.add(item);
+			}
+		}
+		return res;
+	}
+
+	private static void displayList(List<List<String>> suggestions) {
+		for (List<String> suggestion : suggestions) {
+			System.out.println(suggestion);
+		}
+		
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
+		String[] words = { "mobile", "mouse", "moneypot", "monitor", "mousepad" };
 
+		String searchWord = "mouse";
+
+		List<List<String>> suggestions = suggestedProducts(words, searchWord);
+		
+		displayList(suggestions);
+		
+		words = new String[] { "havana" };
+
+		searchWord = "havana";
+		
+		suggestions = suggestedProducts(words, searchWord);
+		
+		displayList(suggestions);
+		
+		
 	}
 
 }
