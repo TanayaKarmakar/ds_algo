3d672f25bb45833ed25a9d0568eb166cb8910f7c
Tanaya Karmakar <karmakar.tanaya@gmail.com>
2020-09-08 11:56:34 +0530

    code

--- leetcode-practice/src/com/app/practice/array/TicTacToeLeetcode348.java
+++ leetcode-practice/src/com/app/practice/array/TicTacToeLeetcode348.java
@@ -3,69 +3,81 @@ package com.app.practice.array;
 class TicTacToe {
 	char[][] board;
 	int size;
-	
-	public TicTacToe(int n) {
-		this.board = new char[n][n];
-		this.size = n;
-	}
-	
-	public int move(int row, int col, int player) {
-		char symbol = (player == 1 ? 'X' : '0');
-		board[row][col] = symbol;
-		if(hasWon(row, col, symbol))
-			return player;
-		return 0;
-	}
-	
-	private boolean hasWon(int row, int col, char symbol) {
-		boolean hasWon = true;
-		for(int i = 0; i < size; i++) {
-			if(board[row][i] != symbol) {
-				hasWon = false;
-				break;
-			}
-		}
-		
-		if(hasWon)
-			return true;
-		
-		for(int i = 0; i < size; i++) {
-			if(board[i][col] != symbol) {
-				hasWon = false;
-				break;
-			}
-		}
-		
-		if(hasWon)
-			return true;
-		
-		for(int i = 0; i < size; i++) {
-			if(board[i][i] != symbol) {
-				hasWon = false;
-				break;
-			}
-		}
-		
-		if(hasWon)
-			return true;
-		
-		for(int i = 0; i < size; i++) {
-			if(board[i][size - i - 1] != symbol) {
-				hasWon = false;
-				break;
-			}
-		}
-		
-		if(hasWon)
-			return true;
-		return false;
-	}	
+
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+    	size = n;
+        board = new char[size][size];
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+    	char symbol = (player == 1 ? 'X': '0');
+        board[row][col] = symbol;
+        if(hasWon(row, col, symbol))
+        	return player;
+        return 0;
+    }
+    
+    private boolean hasWon(int row, int col, char symbol) {
+    	boolean hasWon = true;
+    	for(int i = 0; i < size; i++) {
+    		if(board[row][i] != symbol){
+    			hasWon = false;
+    			break;
+    		}
+    	}
+    	
+    	if(hasWon)
+    		return true;
+    	hasWon = true;
+    	for(int i = 0; i < size; i++) {
+    		if(board[i][col] != symbol) {
+    			hasWon = false;
+    			break;
+    		}
+    	}
+    	
+    	if(hasWon)
+    		return true;
+    	hasWon = true;
+    	
+    	for(int i = 0; i < size; i++) {
+    		if(board[i][i] != symbol) {
+    			hasWon = false;
+    			break;
+    		}
+    	}
+    	
+    	if(hasWon)
+    		return true;
+    	hasWon = true;
+    	
+    	for(int i = 0, j = size - 1; i < size && j >= 0; i++, j--) {
+    		if(board[i][j] != symbol) {
+    			hasWon = false;
+    			break;
+    		}
+    	}
+    	
+    	if(hasWon)
+    		return true;
+    	return false;
+    }
 }
+
 
 public class TicTacToeLeetcode348 {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 
 	}
 
--- leetcode-practice/src/com/app/practice/dp/PerfectSquaresLeetcode279.java
+++ leetcode-practice/src/com/app/practice/dp/PerfectSquaresLeetcode279.java
@@ -1,47 +1,36 @@
 package com.app.practice.dp;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class PerfectSquaresLeetcode279 {
-	private static final int INT_MAX = 1000000006;
-
-	private static int numSquaresMemo(Integer[][] memo, int n, int i) {
-		if (n < 0 || i == 0)
-			return INT_MAX;
-		if (n == 0)
-			return 0;
-		if (memo[n][i] == null) {
-			int option1 = 1 + numSquaresMemo(memo, n - (i * i), i);
-			int option2 = numSquaresMemo(memo, n, (i - 1));
-			memo[n][i] = Integer.min(option1, option2);
-		}
-		return memo[n][i];
-	}
-
 	private static int numSquares(int n) {
-		int maxBoundary = (int) Math.sqrt(n);
-		Integer[][] memo = new Integer[n + 1][maxBoundary + 1];
-		int num = numSquaresMemo(memo, n, maxBoundary);
-		System.out.println(Arrays.deepToString(memo));
-		return num;
-	}
-	
-	private static int numSquares1(int n) {
 		int[] dp = new int[n + 1];
-		for(int i = 1; i <= n; i++) {
+		int sqrt = (int) Math.sqrt(n);
+
+		for (int i = 1; i <= n; i++) {
 			dp[i] = i;
-			for(int j = 1; (i - (j * j)) >=0; j++) {
-				dp[i] = Integer.min(1 + dp[i - (j * j)], dp[i]);
+			int subRes = i;
+			for (int j = 0; j <= sqrt; j++) {
+				if (i >= (j * j)) {
+					subRes = Integer.min(subRes, dp[i - (j * j)]);
+				}
 			}
+			if (subRes != i)
+				dp[i] = Integer.min(subRes + 1, dp[i]);
 		}
-		
 		return dp[n];
 	}
 
 	public static void main(String[] args) {
-		int n = 12;
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println(numSquares1(n));
+		int n = scanner.nextInt();
+		int ans = numSquares(n);
+		
+		System.out.println(ans);
+		
+		scanner.close();
+
 	}
 
 }
